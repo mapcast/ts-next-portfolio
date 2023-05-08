9189e2e527fe7e038ee2dabadcb44963df5c7145
mapcast <36078799+mapcast@users.noreply.github.com>
2023-05-08 17:23:45 +0900

    Update skill.tsx

--- components/skill.tsx
+++ components/skill.tsx
@@ -3,6 +3,13 @@ export default class Skill {
     _file_src: string;
     _img_width: string;
     _img_height: string;
+    
+    constructor(_modal_content: string, _file_src: string, _img_width: string, _img_height: string) {
+        this._modal_content = _modal_content;
+        this._file_src = _file_src;
+        this._img_width = _img_width;
+        this._img_height = _img_height;
+    }
 
     set modal_content(value) {
         this._modal_content = value;
